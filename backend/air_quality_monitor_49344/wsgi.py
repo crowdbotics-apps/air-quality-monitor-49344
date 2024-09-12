5ea1da81419bfa6e0016e42763d5e3776feee462
Crowdbotics <team@crowdbotics.com>
2024-09-12 09:52:13 +0000

    Initial commit

---
+++ backend/air_quality_monitor_49344/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for air_quality_monitor_49344 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "air_quality_monitor_49344.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "air-quality-monitor-49344",
+  url: "https://air-quality-monitor-49344.botics.co",
+  api: "https://air-quality-monitor-49344.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
